c9022506680708f579995d83697f867e90a5bdb7
Pedro502500 <pedro.2001@live.com.mx>
2021-10-09 15:38:57 -0500

    Se modifican objetos para cumplir con el DRY

---
+++ src/main/java/JavaEEJDBC/Libro.java
@@ -0,0 +1,106 @@
+package JavaEEJDBC;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class Libro {
+
+	private int NumLib;
+	private String ISBN;
+	private String Titulo;
+	private int Categoria;
+	private float Precio;
+
+	public Libro() {
+
+	}
+
+	public Libro(String iSBN, String titulo, int categoria, float precio) {
+		ISBN = iSBN;
+		Titulo = titulo;
+		Categoria = categoria;
+		Precio = precio;
+	}
+
+	public String getISBN() {
+		return ISBN;
+	}
+
+	public void setISBN(String iSBN) {
+		ISBN = iSBN;
+	}
+
+	public String getTitulo() {
+		return Titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		Titulo = titulo;
+	}
+
+	public int getCategoria() {
+		return Categoria;
+	}
+
+	public void setCategoria(int categoria) {
+		Categoria = categoria;
+	}
+
+	public float getPrecio() {
+		return Precio;
+	}
+
+	public void setPrecio(float precio) {
+		Precio = precio;
+	}
+
+	public int getNumLib() {
+		return NumLib;
+	}
+
+	public ArrayList<Integer> buscarLasCategorias() {
+		String consultaSQL = "SELECT DISTINCT(cat_lib) FROM libros";
+		DataBaseHelper dbh = new DataBaseHelper();
+		ResultSet rs = dbh.seleccionarRegistros(consultaSQL);
+		ArrayList<Integer> listaCategorias = new ArrayList<Integer>();
+		try {
+			while(rs.next())
+			{
+				listaCategorias.add(rs.getInt("cat_lib"));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return listaCategorias;
+	}
+
+	public int insertar() {
+		String consultaSQL = "INSERT INTO libros(isbn_lib, tit_lib, cat_lib, pre_lib) VALUES";
+		consultaSQL += "('" + ISBN + "','" + Titulo + "'," + Categoria + "," + Precio + ")";
+		DataBaseHelper dbh = new DataBaseHelper();
+		int filas = dbh.modificarRegistro(consultaSQL);
+		dbh.cerrarObjetos();
+		return filas;
+	}
+
+	public ArrayList<Libro> consultarLibros() {
+		String SQL = "SELECT * FROM libros";
+		DataBaseHelper dbh = new DataBaseHelper();
+		ResultSet rs = dbh.seleccionarRegistros(SQL);
+		ArrayList<Libro> ListaDeLibros = new ArrayList<Libro>();
+		try {
+			while(rs.next())
+			{
+				Libro lib = new Libro(rs.getString("isbn_lib"), rs.getString("tit_lib"), rs.getInt("cat_lib"),
+						rs.getFloat("pre_lib"));
+				lib.NumLib = rs.getInt("num_lib");
+				ListaDeLibros.add(lib);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		dbh.cerrarObjetos();
+		return ListaDeLibros;
+	}
+}
--- src/main/webapp/MostrarLibro.jsp
+++ src/main/webapp/MostrarLibro.jsp
@@ -1,27 +1,58 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
 <%@ page import="java.sql.ResultSet"%>
+<%@ page import="java.sql.SQLException"%>
+<%@ page import="java.util.ArrayList"%>
 <%@ page import="JavaEEJDBC.DataBaseHelper"%>
+<%@ page import="JavaEEJDBC.Libro"%>
 <html lang="es">
 <head>
 <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+  <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
 <title>Insert title here</title>
 </head>
 <body>
-
-	<%
-	String SQL = "SELECT * FROM libros";
-	DataBaseHelper dbh = new DataBaseHelper();
-	ResultSet rs = dbh.seleccionarRegistros(SQL);
-	while (rs.next()) {
-	%>
-	<%=rs.getInt("num_lib")%>
-	<%=rs.getString("isbn_lib")%>
-	<%=rs.getString("tit_lib")%>
-	<%=rs.getInt("cat_lib")%>
-	<%=rs.getFloat("pre_lib")%>
+	<h1>Selecciona por Categoria: </h1>
+	<div>
+		<select name="categoria" class="form-select form-select-lg mb-3">
+			<option value="0">Seleccionar</option>
+			<%
+					ArrayList<Integer> listaCategoria = new  Libro().buscarLasCategorias();
+					for(Integer c: listaCategoria ) {%>
+						<option value="<% c.intValue(); %>"> <%= c.intValue()%> </option>
+					<%}
+			%>
+		</select>
+	</div>
+	<div>
+		<table class="table">
+			<thead class="thead-dark">
+				<tr>
+					<th scope="col">#</th>
+					<th scope="col">ISBN</th>
+					<th scope="col">Titulo</th>
+					<th scope="col">Categoria</th>
+					<th scope="col">Precio</th>
+				</tr>
+			</thead>
+			<tbody>
+				<%
+					ArrayList<Libro> libros = new Libro().consultarLibros();
+					for(Libro lib: libros){
+					%>
+				<tr>
+					<th scope="row"><%=lib.getNumLib() %></th>
+					<td><%=lib.getISBN()%></td>
+					<td><%=lib.getTitulo()%></td>
+					<td><%=lib.getCategoria()%></td>
+					<td><%=lib.getPrecio() %></td>
+				</tr>
+				<%} %>
+			</tbody>
+		</table>
+	</div>
 	<br />
-	<%} %>
 	<a href="FormularioInsertarLibro.jsp">Inserta un Libro</a>
 </body>
 </html>
